5b90bd8605d172dd4d6742d93e03aa6145aaa232
hugosantos-uf <140002884+hugosantos-uf@users.noreply.github.com>
2023-10-25 19:11:09 -0300

    Create Observable.java

---
+++ anotacao/Observable.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Observable {
+    private String identificador;
+    private List<Object> observers;
+
+    public Observable(String identificador) {
+        this.identificador = identificador;
+        this.observers = new ArrayList<>();
+    }
+
+    public void addObserver(Object observer) {
+        if (observer.getClass().isAnnotationPresent(Observer.class)) {
+            Observer annotation = observer.getClass().getAnnotation(Observer.class);
+            if (annotation.observado().equals(identificador)) {
+                observers.add(observer);
+            } else {
+                throw new IllegalArgumentException("O identificador do observer não coincide com o identificador do objeto observado.");
+            }
+        } else {
+            throw new IllegalArgumentException("A classe do observer não está marcada com a anotação @Observer.");
+        }
+    }
+
+    // Restante do código do objeto observado...
+}
